8e85630892a640292bfc3215dd478ba021d82195
HarrisHu2011 <25175@student.macleans.school.nz>
2025-03-27 18:58:52 +1300

    Create my homework.py

---
+++ my homework.py
@@ -0,0 +1,32 @@
+#question 1
+minutes1 = int(30)
+seconds1 = minutes1 * 60
+print(seconds1, "seconds")
+
+minutes2 = int(5)
+seconds2 = minutes2 * 60
+print(seconds2, "seconds")
+
+#question 2
+Cruella = int(7)
+Jane = Cruella * 2
+print("Jane receives at least" ,Jane, "cards" )
+
+Cruella = int(59)
+Jane = Cruella * 2
+print("Jane receives at least" ,Jane, "cards" )
+
+#question 3
+for i in range(2):
+   a = int(input("enter your age:"))
+   Days = a * 365
+   print("You are" ,Days, "days old.")
+
+#question 3.1
+a = int(input("enter your age:"))
+Days = a * 365
+b = 100 * 365
+c = b  - Days
+print("You are" ,Days, "days old.")
+print("In", c, "days, you will be 100 years old!")
+
